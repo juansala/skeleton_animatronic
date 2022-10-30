8a26c05e814660f559ccfb279bd32ee02fa50088
Juan Salazar <juansal1204@gmail.com>
2022-10-30 13:06:22 -0400

    added picam test script and updated face_track.py

--- src/face_track.py
+++ src/face_track.py
@@ -1,52 +1,42 @@
 import io
 import picamera
 import cv2
-import numpy
+import numpy as np
 
-#Create a memory stream so photos doesn't need to be saved in a file
-stream = io.BytesIO()
+cap = cv2.VideoCapture(0)
+cap.set(3,160) # set Width
+cap.set(4,120) # set Height
 
-#video_capture = cv2.VideoCapture(0)
+#Load a cascade file for detecting faces
+face_cascade = cv2.CascadeClassifier('/home/pi/skeleton_animatronic/xml/face_file.xml')
+
 
 while True:
 
-    #ret, frames = video_capture.read()
-
-    #Get the picture (low resolution, so it should be quite fast)
-    #Here you can also specify other parameters (e.g.:rotate the image)
-    with picamera.PiCamera() as camera:
-        camera.resolution = (320, 240)
-        camera.capture(stream, format='jpeg')
-
-    #Convert the picture into a numpy array
-    buff = numpy.fromstring(stream.getvalue(), dtype=numpy.uint8)
-
-    #Now creates an OpenCV image
-    image = cv2.imdecode(buff, 1)
-
-    #Load a cascade file for detecting faces
-    face_cascade = cv2.CascadeClassifier('/home/pi/skeleton_animatronic/xml/face_file.xml')
-
-    #Convert to grayscale
-    gray = cv2.cvtColor(image,cv2.COLOR_BGR2GRAY)
+    ret, frame = cap.read()
+    #frame = cv2.flip(frame, -1) # Flip camera vertically
+    gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
 
     #Look for faces in the image using the loaded cascade file
     faces = face_cascade.detectMultiScale(gray, 1.1, 5)
 
     print("Found "+str(len(faces))+" face(s)")
 
+    if np.any(faces):
+        print(str(faces[0][0]) + " " + str(faces[0][1]))
+
     #Draw a rectangle around every found face
     for (x,y,w,h) in faces:
-        cv2.rectangle(image,(x,y),(x+w,y+h),(255,255,0),2)
+        cv2.rectangle(frame,(x,y),(x+w,y+h),(255,255,0),2)
 
     #Save the result image
     #cv2.imwrite('result.jpg',image)
 
     #Show the result image
-    cv2.imshow("Face", image)
+    #cv2.imshow("Face", frame)
 
-    if cv2.waitKey(1) == 27:
-        break
+    #if cv2.waitKey(1) == 27:
+    #    break
 
-capture.release()
+cap.release()
 cv2.destroyAllWindows()
